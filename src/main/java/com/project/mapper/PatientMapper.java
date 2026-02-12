fa714a69abcb903009d10f56b432a668a007a4a0
Naveen1854 <kusuma.naveen1854@gmail.com>
2026-02-12 23:54:15 +0530

    hms backend

---
+++ src/main/java/com/project/mapper/PatientMapper.java
@@ -0,0 +1,22 @@
+package com.project.mapper;
+
+import com.project.dto.PatientDto;
+import com.project.entity.Patient;
+
+public class PatientMapper {
+
+	public static PatientDto mapToDto(Patient patient) {
+//		implement your code here
+		return null;
+	}
+	
+	
+	
+	
+	
+
+	public static Patient mapToDto(PatientDto patientDto) {
+//		implement your code here
+		return null;
+	}
+}
